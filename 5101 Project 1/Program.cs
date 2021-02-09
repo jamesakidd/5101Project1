d12c2ebbf9f653b501a45e47f0fad67bb6c9ae0d
James Kidd <garlicraspberry@hotmail.com>
2021-02-08 20:41:19 -0500

    Add project files.

---
+++ 5101 Project 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _5101_Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Nerds!");
+        }
+    }
+}
