c6af4d56187d8d5b1865e3d5ebbfbdf10a9781f1
scullyjms <scullyjms@gmail.com>
2021-02-13 15:21:01 -0500

    Implementation of the client, needs some things cleaned up and still need to test some function calls but the overal implementation is pretty well defined

--- 5101 Project 1/Program.cs
+++ 5101 Project 1/Program.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿/**
+ * Program.cs
+ * Date: 02/13/2021
+ * The main driving/console/client program that will allow users to query
+ * the dataset.
+ */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
-
 
 namespace _5101_Project_1
 {
@@ -21,102 +26,199 @@ namespace _5101_Project_1
             Console.WriteLine("Fetching list of available file names to be processed and queried...\n");
             Console.WriteLine(util.GetFiles());
 
-            // Validate which type of file we want to parse
-            Console.Write("Select an option from the list above (e.g. 1, 2,): ");
-            bool isValid = false;
-            int selection = 0;
-            while (!isValid) {
+            bool isReset = false;
 
-                // Read input
-                string input = Console.ReadLine();
+            while (!isReset) {
 
-                // Check if we got a number
-                try {
-                    selection = Int32.Parse(input);
-                }
-                catch (Exception ex) {
-                    Console.WriteLine("Invalid Input: " + ex.Message);
-                    continue;
-                }
 
-                // Check if number is in range
-                if (selection < 1 || selection > util.files.Length) {
-                    Console.WriteLine("Invalid Input: selection is not in range");
-                }
-                else {
-                    isValid = true;
-                }
-            }
+                // Validate which type of file we want to parse
+                Console.Write("Select an option from the list above (e.g. 1, 2,): ");
+                bool isValid = false;
+                int selection = 0;
+                while (!isValid) {
 
-            // Build the catalogue **** I think I'm doing this right?
-            Statistics stats = new Statistics(util.files[selection - 1].FullName, util.files[selection - 1].Extension);
-            Console.WriteLine("A city catalogue has now been populated from the " + util.files[selection - 1].Name + " file.\n");
+                    // Read input
+                    string input = Console.ReadLine();
 
-            // Ask what type of query we want to run on the catalogue
-            Console.WriteLine("Fetching list of available data querying routines that can be run on the " + util.files[selection - 1].Name + " file.\n");
-            Console.WriteLine(util.DisplayQueries());
-            Console.Write("Select a data query routine from the list above for the " + util.files[selection - 1].Name + " file (e.g. 1, 2,): ");
-
-            // Validate query input
-            isValid = false;
-            int querySelection = 0;
-            while (!isValid) {
-
-                // Read input
-                string input = Console.ReadLine();
-
-                // Check if we got a number
-                try {
-                    querySelection = Int32.Parse(input);
-                }
-                catch (Exception ex) {
-                    Console.WriteLine("Invalid Input: " + ex.Message);
-                    continue;
-                }
-
-                // Check if number is in range
-                if (querySelection < 1 || selection > 6) {
-                    Console.WriteLine("Invalid Input: selection is not in range");
-                }
-                else {
-                    isValid = true;
-                }
-            }
-
-            // Run the query THIS IS STILL IN PROGRESS
-            // ******* STILL IN PROGRESS ****** //
-            switch (querySelection) {
-                case 1:
-                    Console.WriteLine("SELECTED 1");
-                    // This query should print city information,
-                    // Probably need to add a prompt here to ask what city you  information is needed
-                    foreach (KeyValuePair<int, CityInfo> c in stats.CityCatalogue) {
-                        Console.WriteLine(c.Value);
+                    // Check if we got a number
+                    try {
+                        selection = Int32.Parse(input);
                     }
-                    break;
-                case 2:
-                    // Another type of query would go here
-                    Console.WriteLine("Case 2");
+                    catch (Exception ex) {
+                        Console.WriteLine("Invalid Input: " + ex.Message);
+                        continue;
+                    }
 
-                    break;
-                case 3:
-                    Console.WriteLine("Case 3");
-                    break;
-                case 4:
-                    Console.WriteLine("Case 4");
-                    break;
-                case 5:
-                    Console.WriteLine("Case 5");
-                    break;
-                case 6:
-                    Console.WriteLine("Case 6");
-                    break;
-                default:
-                    Console.WriteLine("Default case");
-                    break;
+                    // Check if number is in range
+                    if (selection < 1 || selection > util.files.Length) {
+                        Console.WriteLine("Invalid Input: selection is not in range");
+                    }
+                    else {
+                        isValid = true;
+                    }
+                }
+
+                // Create the catalogue 
+                Statistics stats = new Statistics(util.files[selection - 1].FullName, util.files[selection - 1].Extension);
+                Console.WriteLine("A city catalogue has now been populated from the " + util.files[selection - 1].Name + " file.\n");
+
+                // Ask what type of query we want to run on the catalogue
+                Console.WriteLine("Fetching list of available data querying routines that can be run on the " + util.files[selection - 1].Name + " file.\n");
+                Console.WriteLine(util.DisplayQueries());
+                Console.Write("Select a data query routine from the list above for the " + util.files[selection - 1].Name + " file (e.g. 1, 2,): ");
+
+                // Validate query input
+                isValid = false;
+                int querySelection = 0;
+                while (!isValid) {
+
+                    // Read input
+                    string input = Console.ReadLine();
+
+                    // Check if we got a number
+                    try {
+                        querySelection = Int32.Parse(input);
+                    }
+                    catch (Exception ex) {
+                        Console.WriteLine("Invalid Input: " + ex.Message);
+                        continue;
+                    }
+
+                    // Check if number is in range
+                    if (querySelection < 1 || selection > 6) {
+                        Console.WriteLine("Invalid Input: selection is not in range");
+                    }
+                    else {
+                        isValid = true;
+                    }
+                }
+
+                // ******* Query Selection ****** //
+                // City Info
+                if (querySelection == 1) {
+
+                    bool isDone = false;
+                    // Get city name
+                    while (!isDone) {
+                        Console.Write("Enter a city name: ");
+                        String city = Console.ReadLine();
+
+                        List<CityInfo> info = stats.DisplayCityInformation(city);
+                        if (info.Count == 0) {
+                            Console.WriteLine("Invalid city, please try again.\n");
+                        }
+                        else {
+                            Console.WriteLine("\tName" + info[0].CityName);
+                            Console.WriteLine("\tPopulation: " + info[0].Population);
+                            Console.WriteLine("\tLocation: " + info[0].GetLocation() + "\n");
+                            isDone = true;
+                            isReset = true;
+                        }
+                    }
+                }
+
+                // Display Province Cities
+                if (querySelection == 2) {
+
+                    // Get province name
+                    bool isDone = false;
+                    while (!isDone) {
+
+                        Console.Write("Enter a province name: ");
+                        String province = Console.ReadLine();
+
+                        List<String> cities = stats.DisplayProvinceCities(province);
+                        if (cities.Count == 0) {
+                            Console.WriteLine("Invalid province, please try again.\n");
+                        }
+                        else {
+                            foreach (var c in cities) {
+                                Console.WriteLine("\t" + c);
+                            }
+                            isDone = true;
+                            isReset = true;
+                        }
+                    }
+                }
+                // Calculate Province Population
+                if (querySelection == 3) {
+
+                    bool isDone = false;
+                    while (!isDone) {
+
+                        // Get input
+                        Console.Write("Enter a province name: ");
+                        String province = Console.ReadLine();
+
+                        // Validate input
+                        int population = -1;
+                        population = stats.DisplayProvincePopulation(province);
+                        if (population < 0) {
+                            Console.WriteLine("Invalid province, please try again.\n");
+                            continue;
+                        }
+                        else {
+                            Console.WriteLine("The province of " + province + " has a population of " + population + "\n");
+                            isDone = true;
+                            isReset = true;
+                        }
+                    }
+                }
+                // Match Cities Population
+                if (querySelection == 4) {
+
+                    // Get province name
+                    bool isDone = false;
+                    while (!isDone) {
+
+                        // Get input
+                        Console.Write("Enter two city names, seperated by a comma, to see which city has the larger population (eg. London, Toronto): ");
+                        String input = Console.ReadLine();
+                        String[] cities = input.Split(",");
+
+                        // Validate input
+                        if (cities.Length != 2) {
+                            Console.WriteLine("Invalid input, please try again.\n");
+                            continue;
+                        }
+
+                        /*** UNCOMMENT WHEN FUNCIONTALITY IS INTEGRATED ***/
+                        //CityInfo largerCity = stats.CompareCitiesPopluation(cities[0], cities[1]);
+                        Console.WriteLine("Largest city: + largerCity.name (PLACEHOLDER)");
+                        Console.WriteLine("Population: + largerCity.population (PLACEHOLDER)\n");
+                        isDone = true;
+                        isReset = true;
+                    }
+                }
+
+                // Distance Between Cities
+                if (querySelection == 5) {
+                    bool isDone = false;
+                    while (!isDone) {
+                        Console.Write("Enter two city names, seperated by a comma, to find the distance between them (eg. London, Toronto): ");
+                        String input = Console.ReadLine();
+                        String[] cities = input.Split(",");
+
+                        if (cities.Length != 2) {
+                            Console.WriteLine("Invalid input, please try again.\n");
+                            continue;
+                        }
+
+                        /*** UNCOMMENT WHEN FUNCIONTALITY IS INTEGRATED ***/
+                        //float distance = stats.CalculateDistanceBetweenCities(cities[0], cities[1]);
+                        Console.WriteLine("Distance between cities[0] and cities[1] is distance (PLACEHOLDER)\n");
+                        isDone = true;
+                        isReset = true;
+                    }
+                }
+
+                // Restart Program And Choose Another File or File Type To Query
+                if (querySelection == 6) {
+                    Console.WriteLine("Resetting...\n");
+                }
             }
+            Console.WriteLine("Exiting Program...");
         }
-
     }
-
 }
+
